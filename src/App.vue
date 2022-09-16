61d5caf7c1be9edb7a57fbaeb9ac077fadf0cea5
Ximu-Luya <794876939@qq.com>
2022-09-16 16:06:59 +0800

    页面整体布局自适应

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,23 @@
 <template>
-  <router-view/>
+  <div class="page">
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
+@import url("@/main.scss");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Microsoft YaHei, Helvetica, Arial, sans-serif;
+  background-color: #f7f7f7;
+}
+.page {
+  margin: 0 auto 0 auto;
+  max-width: 600px;
+  height: 100vh;
+  width: 100vw;
+  background-color: white;
+  box-shadow: 0 0 12px 0 rgb(0 0 0 / 10%);
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
-* {
-  box-sizing: border-box;
-}
-img {
-  display: block;
-}
 </style>
