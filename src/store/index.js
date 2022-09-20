0055960f664e0e946621d82887fe3543e81e0e88
Ximu-Luya <794876939@qq.com>
2022-09-20 15:37:14 +0800

    牌堆根据页面容器定位实现 缓存堆最大数量限制与游戏结束实现

--- src/store/index.js
+++ src/store/index.js
@@ -2,52 +2,79 @@ import { createStore } from 'vuex'
 
 // 生产随机数
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //不含最大值，含最小值
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min //不含最大值，含最小值
 }
 
 export default createStore({
   state: {
+    // 总卡牌数据
     allCards: [],
+    // 牌堆
     deck: [],
+    // 缓存堆
     cache: [],
-    cacheBoxPos: {
-      x: 0,
-      y: 0
-    }
+    // 页面容器坐标
+    boxPos: {
+      deckBoxPos: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      },
+      cacheBoxPos: {
+        x: 0,
+        y: 0,
+      },
+    },
+    // 可选项
+    options: {
+      card: {
+        width: 50,
+        height: 60
+      },
+      cacheMax: 6,
+      groupCount: 20,
+      perGroup: 3,
+    },
   },
-  getters: {
-    getDeck(state){
-      return state.deck
-    }
-  },
+  getters: {},
   mutations: {
     /**
      * 初始化缓存堆坐标
-     * @param state 
+     * @param state
      * @param {x, y} pos 坐标
      */
-    initCacheBoxPos(state, pos){
-      state.cacheBoxPos.x = pos.x
-      state.cacheBoxPos.y = pos.y
+    initBoxPos(state, pos) {
+      state.boxPos.deckBoxPos.top = pos.deckBoxPos.top
+      state.boxPos.deckBoxPos.bottom = pos.deckBoxPos.bottom
+      state.boxPos.deckBoxPos.left = pos.deckBoxPos.left
+      state.boxPos.deckBoxPos.right = pos.deckBoxPos.right
+      state.boxPos.cacheBoxPos.x = pos.cacheBoxPos.x
+      state.boxPos.cacheBoxPos.y = pos.cacheBoxPos.y
     },
     /**
      * 初始化牌堆
-     * @param state 
+     * @param state
      * @param {groupCount, perGroup} {牌组数, 每组相同牌数量}
      */
-    initDeck(state, {groupCount=1, perGroup=2}){
-      for(let cardCode=1, id=0; cardCode<=groupCount; cardCode++){
-        for(let cardCount=0; cardCount<perGroup; cardCount++){
+    initDeck(state) {
+      const { boxPos, options } = state
+      state.allCards = []
+      state.deck = []
+      state.cache = []
+
+      for (let cardCode = 1, id = 0; cardCode <= options.groupCount; cardCode++) {
+        for (let cardCount = 0; cardCount < options.perGroup; cardCount++) {
           state.allCards.push({
             id: id++,
             code: cardCode,
             pos: {
-              x: getRandomInt(0, 500),
-              y: getRandomInt(0, 700)
+              x: getRandomInt(boxPos.deckBoxPos.left, boxPos.deckBoxPos.right - options.card.width),
+              y: getRandomInt(boxPos.deckBoxPos.top, boxPos.deckBoxPos.bottom - options.card.height),
             },
-            disappearing: false
+            disappearing: false,
           })
         }
       }
@@ -57,58 +84,70 @@ export default createStore({
     },
     /**
      * 卡牌从牌堆移入缓存堆
-     * @param state 
+     * @param state
      * @param currentCard 卡牌数据
      */
-    moveToCache(state, currentCard){
+    moveToCache(state, currentCard) {
       const target = state.deck.findIndex(item => item.id === currentCard.id)
-      if (target < 0) return alert("该卡牌不存在")
+      if (target < 0) return alert('该卡牌不存在')
 
       state.deck.splice(target, 1)[0]
       state.cache.push(currentCard)
 
       currentCard.pos = {
-        x: state.cacheBoxPos.x + (state.cache.length-1) * 50,
-        y: state.cacheBoxPos.y
+        x: state.boxPos.cacheBoxPos.x + (state.cache.length - 1) * 50,
+        y: state.boxPos.cacheBoxPos.y,
       }
     },
     /**
      * 卡牌打到规定数量进行消除
-     * @param state 
+     * @param state
      * @param {id, code, pos} currentCard 卡牌数据
      */
-    removeSameCard(state, currentCard){
+    removeSameCard(state, currentCard) {
       state.cache = state.cache.filter(item => item.code !== currentCard.code)
       state.allCards = state.allCards.filter(item => item.code !== currentCard.code)
     },
     /**
      * 重新计算缓存堆卡牌的坐标
-     * @param state 
+     * @param state
      */
     rerenderCacheBox(state) {
       state.cache.forEach((item, index) => {
-        item.pos.x = state.cacheBoxPos.x + index * 50
+        item.pos.x = state.boxPos.cacheBoxPos.x + index * 50
       })
     },
   },
   actions: {
-    async saveInCache({state, commit}, currentCard){
+    /**
+     * 点击卡片保存至缓存堆与后序决策
+     * @param context
+     * @param currentCard 当前卡牌数据
+     */
+    async saveInCache({ state, commit }, currentCard) {
       commit('moveToCache', currentCard)
 
       const targets = state.cache.filter(item => item.code === currentCard.code)
-      console.log(targets)
       // 如果该卡牌数量达到3张，返回没有该卡牌的缓存堆，即消除卡牌
-      if(targets.length === 3){
+      if (targets.length === 3) {
         const sameCard = state.allCards.filter(item => item.code === currentCard.code)
-        sameCard.forEach(item => item.disappearing = true)
+        sameCard.forEach(item => (item.disappearing = true))
 
         setTimeout(() => {
           commit('removeSameCard', currentCard)
           commit('rerenderCacheBox')
         }, 1000)
+      } else {
+        // 选择卡牌进入缓存堆后，没有消除卡牌且卡牌数量达到了缓存堆上限，则游戏结束
+        if(state.cache.length === state.options.cacheMax) {
+          setTimeout(() => {
+            alert('游戏结束')
+            commit('initDeck')
+          }, 310);
+          return 
+        }
       }
-    }
+    },
   },
-  modules: {
-  }
+  modules: {},
 })
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,7 +2,12 @@
   <div class="home">
     <card v-for="item in allCards" :key="item" :card-data="item"></card>
     
-    <div class="cache-box"></div>
+    <div class="deck">
+      <div class="deck-box" :style="deckBoxSize"></div>
+    </div>
+    <div class="cache">
+      <div class="cache-box" :style="cacheBoxSize"></div>
+    </div>
   </div>
 </template>
 
@@ -16,19 +21,53 @@ export default {
     CacheBox
   },
   computed: {
-    ...mapState(['allCards'])
+    ...mapState(['allCards', 'options', 'boxPos']),
+    // 牌堆尺寸
+    deckBoxSize(){
+      const { deckBoxPos } = this.boxPos
+      return {
+        width: `${deckBoxPos.right - deckBoxPos.left}px`,
+        height: `${deckBoxPos.bottom - deckBoxPos.top}px`
+      }
+    },
+    // 缓存堆尺寸
+    cacheBoxSize(){
+      const { cacheMax, card } = this.options
+      return {
+        width: `${cacheMax * card.width}px`,
+        height: `${card.height}px`
+      }
+    }
   },
   mounted(){
+    this.initPos()
     // 初始化牌堆
     this.initDeck({groupCount: 3, perGroup: 3})
-    // 获取缓存堆与页面的坐标
-    const cacheBoxPos = document.querySelector(".cache-box").getBoundingClientRect()
-    const pagePos = document.querySelector(".page").getBoundingClientRect()
-    // 初始化缓存堆相对页面的坐标
-    this.initCacheBoxPos({x: cacheBoxPos.x - pagePos.x, y: cacheBoxPos.y})
   },
   methods: {
-    ...mapMutations(['initDeck', 'initCacheBoxPos']),
+    ...mapMutations(['initDeck', 'initBoxPos']),
+    /**
+     * 初始化页面各容器坐标
+     */
+    initPos(){
+      // 获取缓存堆与页面的坐标
+      const pagePos = document.querySelector(".page").getBoundingClientRect()
+      const cacheBoxPos = document.querySelector(".cache-box").getBoundingClientRect()
+
+      // 初始化缓存堆相对页面的坐标
+      this.initBoxPos({
+        deckBoxPos: {
+          top: 50,
+          bottom: cacheBoxPos.y - 50,
+          left: 50,
+          right: pagePos.width - 50,
+        },
+        cacheBoxPos: {
+          x: cacheBoxPos.x - pagePos.x,
+          y: cacheBoxPos.y
+        },
+      })
+    }
   }
 }
 </script>
@@ -39,11 +78,19 @@ export default {
   width: 100%;
   height: 100%;
 
-  .cache-box{
+  .deck{
+    position: absolute;
+    top: 40px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 0;
+    padding: 10px;
+    
+    border: 2px solid #606266;
+  }
+  .cache{
     position: absolute;
     bottom: 40px;
-    width: 75%;
-    height: 60px;
     left: 50%;
     transform: translateX(-50%);
     border: 2px solid #606266;
