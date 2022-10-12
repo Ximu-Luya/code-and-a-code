55b368c534fdd69be0562a5bc4f2b024146b7652
Ximu-Luya <794876939@qq.com>
2022-10-12 20:25:28 +0800

    新增打包命令，一键打包为指定文件夹名称

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const outputdir = process.env.outputdir || 'release'
 module.exports = defineConfig({
+  outputDir: outputdir,
   publicPath: './',
   transpileDependencies: true
 })
